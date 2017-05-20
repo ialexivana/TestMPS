66f2ef6435b274702037aab4f9ad6fd404a6f81c
ivana <ivana@adobe.com>
2017-05-20 08:45:24 +0300

    Fixing main test

--- src/main/java/com/main/MainApp.java
+++ src/main/java/com/main/MainApp.java
@@ -20,7 +20,6 @@ public class MainApp {
     public void init() {
         camera = new Camera();
         semafor = new Semafor();
-
     }
 
     public static void main(String[] args) {
@@ -29,5 +28,45 @@ public class MainApp {
 
     public String writeHello(String name) {
         return "Hi " + name;
+    }
+
+    public Camera getCamera() {
+        return camera;
+    }
+
+    public void setCamera(Camera camera) {
+        this.camera = camera;
+    }
+
+    public Semafor getSemafor() {
+        return semafor;
+    }
+
+    public void setSemafor(Semafor semafor) {
+        this.semafor = semafor;
+    }
+
+    public Monitoring getMonitoring() {
+        return monitoring;
+    }
+
+    public void setMonitoring(Monitoring monitoring) {
+        this.monitoring = monitoring;
+    }
+
+    public ICamera getIcamera() {
+        return icamera;
+    }
+
+    public void setIcamera(ICamera icamera) {
+        this.icamera = icamera;
+    }
+
+    public ISemafor getIsemafor() {
+        return isemafor;
+    }
+
+    public void setIsemafor(ISemafor isemafor) {
+        this.isemafor = isemafor;
     }
 }
